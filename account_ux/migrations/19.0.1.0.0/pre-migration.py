f1d2c8867e366e9341b2ec728aa977dcfd41afaf
pmolladynetis <pablo.molla@dynetis.com>
2026-05-24 16:44:04 +0000

    Add settled_line_ids cleanup to pre-migration

--- account_ux/migrations/19.0.1.0.0/pre-migration.py
+++ account_ux/migrations/19.0.1.0.0/pre-migration.py
@@ -7,3 +7,10 @@ def migrate(cr, version):
         WHERE name IN ('account.payment.tree', 'account.payment.tree.personalization')
         AND arch_db::text LIKE '%payment_method_description%'
     """)
+
+    # Remove views with settled_line_ids field that no longer exists in v19
+    cr.execute("""
+        UPDATE ir_ui_view 
+        SET arch_db = '{"en_US": "<data/>", "es_AR": "<data/>"}'::jsonb
+        WHERE arch_db::text LIKE '%settled_line_ids%'
+    """)
