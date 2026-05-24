09d9fca658407a447249a71a0a4e7ba370245261
pmolladynetis <pablo.molla@dynetis.com>
2026-05-24 14:16:27 +0000

    Fix post-migration signature: use (cr, version) instead of (env, version)

--- account_ux/migrations/19.0.1.5.0/post-migration.py
+++ account_ux/migrations/19.0.1.5.0/post-migration.py
@@ -1,16 +1,19 @@
-def migrate(env, version):
+def migrate(cr, version):
     """
     Update account.journal_comp_rule domain_force
     even if the rule is marked as noupdate.
     """
-    rule = env.ref("account.journal_comp_rule", raise_if_not_found=False)
-    if not rule:
-        return
-    new_domain = """[
-        '|',
-        ('company_id', 'in', company_ids),
-        '&',
-        ('company_id', 'parent_of', company_ids),
-        ('shared_to_branches', '=', True)
-    ]"""
-    rule.write({"domain_force": new_domain})
+    cr.execute("""
+        UPDATE ir_rule
+        SET domain_force = '[
+            "|\",
+            (\"company_id\", \"in\", company_ids),
+            \"&\",
+            (\"company_id\", \"parent_of\", company_ids),
+            (\"shared_to_branches\", \"=\", True)
+        ]'
+        WHERE id = (
+            SELECT res_id FROM ir_model_data 
+            WHERE module = 'account' AND name = 'journal_comp_rule'
+        )
+    """)
